55b20cb7f02fd2a4ef909276c29388fdfa770194
MostafaNouh0011 <mostafanouh481@gmail.com>
2024-04-28 18:55:48 +0300

    SELECT Statement

--- Select.sql
+++ Select.sql
@@ -11,26 +11,3 @@ FROM EmployeeDemographics;
 
 SELECT FirstName, LastName  -- Select FirstName and LastName columns
 FROM EmployeeDemographics;
-
-/*
-DISTINCT, COUNT, MAX, MIN, AVG, SUM Functions
-
-*/
-
-SELECT DISTINCT(EmployeeID)    -- Returns unique values in this row
-FROM EmployeeDemographics;
-
-SELECT COUNT(LastName)         -- Returns number of rows in this column
-FROM EmployeeDemographics;
-
-SELECT MAX(Salary)             -- Returns the largest value of the selected column
-FROM EmployeeSalary;
-
-SELECT MIN(Salary)             -- Returns the smallest value of the selected column
-FROM EmployeeSalary;
-
-SELECT AVG(Salary)             -- Returns the average value of a numeric column
-FROM EmployeeSalary;
-
-SELECT SUM(Salary)             -- Returns the total sum of a numeric column
-FROM EmployeeSalary;  
